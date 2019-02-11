e485c1a721336471c09b6e70222d295c500f1bda
Adam Howell <adam.j.howell@gmail.com>
2019-02-11 12:18:22 -0700

    3 - Set up the project in CLion

---
+++ cmake-build-debug/CMakeFiles/CSV_Parser.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CSV_Parser.dir/src/main.c.o"
+  "CMakeFiles/CSV_Parser.dir/src/csvparser.c.o"
+  "CSV_Parser.pdb"
+  "CSV_Parser"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/CSV_Parser.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
